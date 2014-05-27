f868cc30e834d39454738ad8084638862c07783e
Ranga Gamage <grpkarunathilaka>
2014-05-27 22:01:30 +1000

    Added backend code

--- Gramophone.Web/Models/Services/SongService.cs
+++ Gramophone.Web/Models/Services/SongService.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Gramophone.Web.Models.Services
 {
     public class SongService
     {
         public void AddSong(SongDTO song)
-        { 
-        //todo
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=CD-RANGAK;Initial Catalog=dbTest;Integrated Security =True"))
+            {
+                using (SqlCommand cmd=connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "";
+                    cmd.Parameters.AddWithValue("@SongTitle", song.SongTitle);
+                }
+                
+            }
         }
     }
 }
